cca8cbbeaf7bad27e8f7c352ec955d8296a92665
Shourya <05shourya@gmail.com>
2024-08-09 00:58:17 +0530

    .

--- src/profilePic.tsx
+++ src/profilePic.tsx
@@ -2,10 +2,7 @@ export default function ProfilePic() {
     return (
         <>
             <div className="profilePic">
-                <img
-                    src="../public/Photos/profilePhoto.jpeg"
-                    alt="Couldn't show image"
-                />
+                <img src="Photos/profilePhoto.jpeg" alt="Couldn't show image" />
             </div>
         </>
     );
