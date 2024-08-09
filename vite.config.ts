783a6325c18c0f97b773abb4b25ad4de55671feb
Shourya <05shourya@gmail.com>
2024-08-09 15:58:22 +0530

    Changed base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	base: "/portfolio", 
-  plugins: [react()],
-})
+// export default defineConfig({
+// 	base: mode === 'production' ? '/portfolio/' : '/', 
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ mode }) => ({
+	base: mode === 'production' ? '/portfolio/' : '/',
+	plugins: [react()],
+  }));
