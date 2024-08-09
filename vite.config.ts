dcd45df07cd806506b1c34a0bad4d88d25246023
Shourya <05shourya@gmail.com>
2024-08-09 15:59:23 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ import react from '@vitejs/plugin-react'
 // })
 
 export default defineConfig(({ mode }) => ({
-	base: mode === 'production' ? '/portfolio/' : '/',
+	// base: mode === 'production' ? '/portfolio/' : '/',
 	plugins: [react()],
   }));
